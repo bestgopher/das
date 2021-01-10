2c6ab3b858028dd6e0a267796bdc395a4e4f6a16
bestgopher <84328409@qq.com>
2021-01-10 16:40:59 +0800

    add recursion/linear/reverse

--- recursion/linear.py
+++ recursion/linear.py
@@ -5,6 +5,13 @@ def linear_sum(s, n):
     return s[-1] + linear_sum(s[:n - 1], n - 1)
 
 
+def reverse(s, start, stop):
+    """Reverse elements in implicits slice s[start, end]"""
+    if start < stop - 1:
+        s[start], s[stop - 1] = s[stop - 1], s[start]
+        reverse(s, start + 1, stop - 1)
+
+
 if __name__ == '__main__':
     def main():
         assert 45 == linear_sum(list(range(10)), 10)
