b89853cdedbd3b6df250bac260781f9c4fc5e633
bestgopher <84328409@qq.com>
2021-01-15 20:26:35 +0800

    add a capacity in ArrayStack

---
+++ stack_queue/stack.py
@@ -0,0 +1,48 @@
+from stack_queue.exceptions import Empty
+
+
+class ArrayStack:
+    """LIFO Stack implementation using a Python list as underlying storage."""
+
+    def __init__(self, capacity=0):
+        """Create an empty stack."""
+        # self._data = []
+        self._num = 0  # the number of element
+        self._data = [None] * capacity
+
+    def __len__(self):
+        """Return the number of elements in the stack."""
+        # return len(self._data)
+        return self._num
+
+    def is_empty(self):
+        """Return True if the stack is empty."""
+        return len(self) == 0
+
+    def push(self, e):
+        """Add element e to the top of the stack."""
+        if len(self._data) > self._num:
+            self._data[self._num] = e
+        else:
+            self._data.append(e)  # new item stored at end of list.
+
+        self._num += 1
+
+    def top(self):
+        """
+        Return (but do not remove) the element at the top of the stack.
+        Raise Empty exception if the stack is empty.
+        """
+        if self.is_empty():
+            raise Empty("Stack is empty.")
+        return self._data[-1]
+
+    def pop(self):
+        """
+        Remove and return the element from the top of the stack.(i.e., LIFO).
+        Raise Empty exception if the stack is empty.
+        """
+        if self.is_empty():
+            raise Empty("Stack is empty.")
+        self._num -= 1
+        return self._data.pop()
